2f2e4c703d63ffe8df08fea8304e8300a8188be1
QFZaman <quazi.fahmiduzzaman@gmail.com>
2022-07-13 00:40:15 +0600

    Fixed some UI issues

--- todo.js
+++ todo.js
@@ -1,63 +1,51 @@
-function get_todos()
-{
-    var todos = new Array;
-    var todos_str = localStorage.getItem('todo');
-    if (todos_str !== null)
-    {
-        todos = JSON.parse(todos_str);
+function get_todos() {
+    var todos = new Array()
+    var todos_str = localStorage.getItem('todo')
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str)
     }
-    return todos;
+    return todos
 }
-function add()
-{
-    var task = document.getElementById('input_btn').value;
-	if (task == ""){
-		alert("Task Namee must be filled out");
-		
-	}
-	else{
-		 var todos = get_todos();
-		todos.push(task);
-		localStorage.setItem('todo', JSON.stringify(todos));
+function add() {
+    var task = document.getElementById('input_btn').value
+    if (task == '') {
+        alert('Task Namee must be filled out')
+    } else {
+        var todos = get_todos()
+        todos.push(task)
+        localStorage.setItem('todo', JSON.stringify(todos))
 
-		show();
-	}
-   
-    return false;
+        show()
+    }
+
+    return false
 }
-function clearDeafult(a)
-{
-    if (a.defaultValue == a.value)
-    {
-        a.value = ""
-        }
-};
-function remove()
-{
-    var id = this.getAttribute('id');
-    var todos = get_todos();
-    todos.splice(id, 1);
+function clearDeafult(a) {
+    if (a.defaultValue == a.value) {
+        a.value = ''
+    }
+}
+function remove() {
+    var id = this.getAttribute('id')
+    var todos = get_todos()
+    todos.splice(id, 1)
     localStorage.setItem('todo', JSON.stringify(todos))
-    show();
+    show()
 
-    return false;
+    return false
 }
-function show()
-{
-    var todos = get_todos();
-    var html = '<ul>';
-    for (var i = 0; i < todos.length; i++)
-    {
-        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '"> <i class="fa fa-plus-square" ></i> Delete</button> </li>';
-    };
+function show() {
+    var todos = get_todos()
+    var html = '<ul>'
+    for (var i = 0; i < todos.length; i++) {
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '"> <i class="fa fa-trash" ></i> Delete</button> </li>'
+    }
     html += '</ul>'
-    
-    document.getElementById('todos').innerHTML = html;
-    var buttons = document.getElementsByClassName('remove');
-    for (var i = 0; i < buttons.length; i++)
-    {
-        buttons[i].addEventListener('click',remove)  
-    };
-    
+
+    document.getElementById('todos').innerHTML = html
+    var buttons = document.getElementsByClassName('remove')
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove)
+    }
 }
-document.getElementById('btn').addEventListener('click', add);
+document.getElementById('btn').addEventListener('click', add)
